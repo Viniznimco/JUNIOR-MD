a5c6a11089b59446e1e65396deae0f3cc7ee142f
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-02-03 12:31:48 +0300

    SNAPDRAGON

--- plugins/del.js
+++ plugins/del.js
@@ -23,6 +23,6 @@ const key = {
         await conn.sendMessage(m.chat, { delete: key })
 } catch(e) {
 console.log(e);
-reply('💎 Sɪʟᴠᴀ Sᴘᴀʀᴋ MD 💎 successful..👨‍💻✅')
+reply('💎 DE JUNIOR MD 💎 successful..👨‍💻✅')
 } 
 })
