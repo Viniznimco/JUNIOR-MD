5eb97d5a91180328efce573a4321e5ac524ba388
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-02-03 12:27:31 +0300

    SNAPDRAGON

--- plugins/couplespp.js
+++ plugins/couplespp.js
@@ -17,7 +17,7 @@ cmd({
   reply: _0x30d3fc
 }) => {
   try {
-    _0x30d3fc("*SILVA SPARK IS 💑 Fetching couple profile pictures...*");
+    _0x30d3fc("*DE JUNIOR IS 💑 Fetching couple profile pictures...*");
     const _0x173643 = await axios.get("https://api.davidcyriltech.my.id/couplepp");
     if (!_0x173643.data || !_0x173643.data.success) {
       return _0x30d3fc("❌ Failed to fetch couple profile pictures. Please try again later.");
@@ -29,7 +29,7 @@ cmd({
         'image': {
           'url': _0x5d4b91
         },
-        'caption': "SILVA SPARK SUGGESTS: 👨 Male Couple Profile Picture"
+        'caption': "DE JUNIOR SUGGESTS: 👨 Male Couple Profile Picture"
       }, {
         'quoted': _0x51cf3f
       });
@@ -39,7 +39,7 @@ cmd({
         'image': {
           'url': _0x5a1e4a
         },
-        'caption': "SILVA SPARK SUGGESTS: 👩 Female Couple Profile Picture"
+        'caption': "DE JUNIOR SUGGESTS: 👩 Female Couple Profile Picture"
       }, {
         'quoted': _0x51cf3f
       });
