0c12317241c2ec3b20f6823384bd5d3c2ac5b862
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-02-26 01:51:14 +0300

    SNAPDRAGON
    Signed-off-by: Viniznimco <clintoncheruiyotlangat@gmail.com>

--- plugins/menu.js
+++ plugins/menu.js
@@ -64,7 +64,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/a1sh4u.mp3' },
+            audio: { url: 'https://files.catbox.moe/qi0jr3.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
