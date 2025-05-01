63edad40adde45c072d333295bebd541adb7df7b
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-05-01 09:36:05 +0300

    JUNIOR LORD 💝
    Signed-off-by: Viniznimco <clintoncheruiyotlangat@gmail.com>

--- command.js
+++ command.js
@@ -1,3 +1,4 @@
+/*
 var commands = [];
 
 function cmd(info, func) {
@@ -19,3 +20,24 @@ module.exports = {
     commands,
 };
 
+*/
+var commands = [];
+
+function cmd(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    cmd,
+    AddCommand:cmd,
+    Function:cmd,
+    Module:cmd,
+    commands,
+};
