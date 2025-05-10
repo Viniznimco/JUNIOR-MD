460d2f00a4f371aede09d670542ae662eee1f398
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-05-10 15:52:37 +0300

    JUNIOR LORD 💝
    Signed-off-by: Viniznimco <clintoncheruiyotlangat@gmail.com>

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ 𒁂𓄂❥.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼࿐ ♡*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ 𒁂𓄂❥.𝑫𝑬 𝑱𝑼𝑵𝑰𝑶𝑹🔥༽༼࿐ ♡*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
